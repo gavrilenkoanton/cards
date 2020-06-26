5a7a4672068e13aa14ac5db407b3910461f6c754
anton <gavrilenko7732@gmail.com>
2020-06-26 15:03:11 +0300

    Updates

---
+++ precache-manifest.bc689bd1293be35d8683a40e97df97ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62090e41bc472fe59c0707093131c643",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "6331e108e294d4f7f961",
+    "url": "/cards/static/css/main.bd89dd5e.chunk.css"
+  },
+  {
+    "revision": "e447e3a58cb1abd00479",
+    "url": "/cards/static/js/2.3800fbb1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards/static/js/2.3800fbb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6331e108e294d4f7f961",
+    "url": "/cards/static/js/main.2beef6fc.chunk.js"
+  },
+  {
+    "revision": "6b603ba11baa6309201d",
+    "url": "/cards/static/js/runtime-main.699f0398.js"
+  }
+]);
