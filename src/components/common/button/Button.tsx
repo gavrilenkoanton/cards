12a12b9b6f7352836daa19adaa63d5754ca6f88c
JeremyWylde <63048608+JeremyWylde@users.noreply.github.com>
2020-06-26 18:44:21 +0300

    added onChange, onClick, value attribute for common component

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -6,7 +6,8 @@ function Button(props: any) {
 
     return (
         <>
-            <button className={`${styles.button} ${props.color === 'red' ? styles.red : styles.blue} `} >
+            <button className={`${styles.button} ${props.color === 'red' ? styles.red : styles.blue} `}
+            onClick={props.onClick} onChange={props.onChange} value={props.value}>
                 {props.description}
             </button>
         </>
