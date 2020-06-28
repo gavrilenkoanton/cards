3b8d4f5ad7ba9a12e56901026d53ad2ab940bec7
NikitaT1 <nik.tati1@gmail.com>
2020-06-28 14:43:01 +0300

    added some base features, such as JSX & form interface. Buttons & input fileds are just simple stubs

--- src/components/2_Login/Login.tsx
+++ src/components/2_Login/Login.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Field, reduxForm} from "redux-form";
 import styles from './Login.module.scss';
+
+const LoginForm = (props: any) => {
+    return (
+        <form className={styles.formWrapper} onSubmit={props.handleSubmit}>
+            <Field placeholder="Name" component={"input"} name={"user_name"}/>
+            <Field placeholder="e-mail"  component={"input"} name={"user_email"}/>
+            <button>send</button>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({form:'contact'})(LoginForm)
 
 
 function Login (props: any) {
+    
+    const [isHidden, setHidden] = useState<boolean>(true)
 
     return (
         <div className={styles.wrapper}>
-            LoginTest
+            <LoginReduxForm/>
         </div>
     );
 }
