b4503aa8053dd64cd07e93321db01b99543cf6ff
NikitaT1 <nik.tati1@gmail.com>
2020-06-29 11:09:39 +0300

    added uploading data into console & made store a window object

--- src/BLL/redux-store.ts
+++ src/BLL/redux-store.ts
@@ -2,14 +2,21 @@ import {createStore, combineReducers, applyMiddleware} from "redux";
 import thunkMiddleware from "redux-thunk"
 import {loginReducer} from "./login-reducer";
 import { registerReducer } from "./register-reducer";
+import { reducer as formReducer } from 'redux-form'
 
 
 
 let reducers = combineReducers({
     login: loginReducer,
+    form: formReducer,
     register: registerReducer
 })
 
 const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+
+
 export default store;
+// @ts-ignore
+window.store = store;
 
--- src/components/2_Login/Login.tsx
+++ src/components/2_Login/Login.tsx
@@ -1,27 +1,45 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Field, reduxForm} from "redux-form";
 import styles from './Login.module.scss';
 
 const LoginForm = (props: any) => {
     return (
-        <form className={styles.formWrapper} onSubmit={props.handleSubmit}>
+        <form onSubmit={props.handleSubmit}>
             <Field placeholder="Name" component={"input"} name={"user_name"}/>
-            <Field placeholder="e-mail"  component={"input"} name={"user_email"}/>
+            <Field placeholder="password"  component={"input"} name={"password"}/>
+            <Field component={"input"} type={"checkbox"} name={"rememberMe"}/>
             <button>send</button>
         </form>
     )
 }
 
-const LoginReduxForm = reduxForm({form:'contact'})(LoginForm)
+const LoginReduxForm = reduxForm({form:'loginForm'})(LoginForm)
 
 
 function Login (props: any) {
-    
-    const [isHidden, setHidden] = useState<boolean>(true)
+    // const [data, setData] = useState<any>({})
+    //
+    // useEffect(() => {
+    //     if(data) {
+    //         callbackTest(data);
+    //     }
+    // }, [])
+
+    let callbackTest = (data:any) => {
+        console.log(data)
+    }
+
+    const onSubmit = (formData: any) => {
+        //props.LoginThunk(formData.email, formData.password)
+        callbackTest(formData)
+
+    }
+
+
 
     return (
         <div className={styles.wrapper}>
-            <LoginReduxForm/>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     );
 }
